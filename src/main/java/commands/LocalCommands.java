103907ee215c331ff0b33c5efaf0492111678bae
threehalves-pi <1.5pi.statshelper@gmail.com>
2021-07-18 11:21:41 -0400

    Removed unused imports

--- src/main/java/commands/LocalCommands.java
+++ src/main/java/commands/LocalCommands.java
@@ -1,13 +1,11 @@
 package commands;
 
 import events.OnStartup;
-import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class LocalCommands {
     public static void registerLocalSlashCommands(CommandListUpdateAction action) {
